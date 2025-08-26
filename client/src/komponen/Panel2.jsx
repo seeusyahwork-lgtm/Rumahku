2b6e09ada044e2b1b1665c9aae782cbb2aa164e5
Syah Putra N <syah.putrawork98@gmail.com>
2025-08-26 10:53:24 +0700

    banyak peruhaban dan mungkin akan berubah kembali nanti

--- client/src/komponen/Panel2.jsx
+++ client/src/komponen/Panel2.jsx
@@ -9,16 +9,16 @@ function Panel2() {
           className="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold">Perancangan dan Perencaan</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Konsultasikan rumah impian anda agar kami bisa membantu dalam perancangan dan Perencaan
+            rumah yang indah adalah rumah yang rancangan dengan perencaan yang matang
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
     </div>
+    
   );
 }
 
